645d079e64bc5c535b82954aafde08bc9088c8df
GiselleBarbosa <g88barbosa@gmail.com>
2023-10-31 00:10:30 -0300

    chore: added template form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { TemplateComponent } from './template/template.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent, TemplateComponent, HomeComponent],
@@ -22,6 +26,10 @@ import { HomeComponent } from './home/home.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
   ],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,87 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: ``,
-})
-export class HomeComponent {
+  styles: [
+    `
+      .form {
+        min-width: 150px;
+        max-width: 500px;
+        width: 100%;
+      }
 
-}
+      .full-width-field {
+        width: 100%;
+      }
+
+      .small-field {
+        width: 50%;
+      }
+    `,
+  ],
+  template: `
+    <form class="p-5">
+      <table class="full-width-field" cellspacing="0">
+        <tr>
+          <td>
+            <mat-form-field class="small-field ">
+              <mat-label>Zip Code</mat-label>
+              <input matInput />
+            </mat-form-field>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <mat-form-field class="full-width-field">
+              <mat-label>Address</mat-label>
+              <input matInput />
+            </mat-form-field>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <mat-form-field class="full-width-field">
+              <mat-label>Complement</mat-label>
+              <input
+                matInput
+                #reference
+                maxlength="50"
+                placeholder="Exemplo: Próximo ao colégio"
+              />
+              <mat-hint align="end">{{ reference.value.length }} / 50</mat-hint>
+            </mat-form-field>
+          </td>
+        </tr>
+      </table>
+
+      <table class="full-width-field" cellspacing="0">
+        <tr>
+          <td>
+            <mat-form-field class="full-width-field">
+              <mat-label>City</mat-label>
+              <input matInput placeholder="Ex. San Francisco" />
+            </mat-form-field>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <mat-form-field class="full-width-field">
+              <mat-label>State</mat-label>
+              <input matInput placeholder="Ex. California" />
+            </mat-form-field>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <mat-form-field class="full-width-field">
+              <mat-label>Country</mat-label>
+              <input matInput placeholder="Ex. California" />
+            </mat-form-field>
+          </td>
+        </tr>
+      </table>
+      <button mat-raised-button color="primary" class="w-100 mt-1">Search</button>
+    </form>
+  `,
+})
+export class HomeComponent {}
