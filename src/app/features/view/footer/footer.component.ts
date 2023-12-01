17d91df7df446f28434a9df9db5554eff00a33cb
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-01 18:57:43 -0300

    feat: added new translations

--- src/app/features/view/footer/footer.component.ts
+++ src/app/features/view/footer/footer.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbarModule],
-  template: ` <mat-toolbar
-    color="secondary"
-    class="d-flex justify-content-center align-items-center"
-  >
-    <p style="font-size: .75rem; font-weight: 100 ">
-      &copy;2023<span style="font-weight: 500; margin-right: 5px"> Zipcode Search </span>
-    </p>
-    <p style="font-size: .75rem; font-weight: 100 ">
-      Developed with ❤️ by Giselle Barbosa
-    </p>
-  </mat-toolbar>`,
+  imports: [MatToolbarModule, TranslocoModule],
+  template: ` <ng-container *transloco="let transloco">
+    <mat-toolbar
+      color="secondary"
+      class="d-flex justify-content-center align-items-center"
+    >
+      <p style="font-size: .75rem; font-weight: 100 ">
+        &copy;2023<span style="font-weight: 500; margin-right: 5px">
+          Zipcode Search
+        </span>
+      </p>
+      <p style="font-size: .75rem; font-weight: 100 ">
+        {{ transloco('footer.developed-with') }}
+        ❤️ {{ transloco('footer.by') }}
+      </p>
+    </mat-toolbar></ng-container
+  >`,
 })
 export class FooterComponent {}
